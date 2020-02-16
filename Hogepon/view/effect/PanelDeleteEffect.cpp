9701515f86b1826b34b978703e0c605a458f1dd5
hogehogei <matsuryo00@gmail.com>
2020-02-16 09:42:49 +0900

    ・パネル消去エフェクトを変更

--- Hogepon/view/effect/PanelDeleteEffect.cpp
+++ Hogepon/view/effect/PanelDeleteEffect.cpp
@@ -24,9 +24,6 @@ void PanelDeleteEffect::Update()
     // 現在位置に移動ベクトルを足す
     m_Move.x += m_Vec.x * m_Speed;
     m_Move.y += m_Vec.y * m_Speed;
-
-    // 少し小さくする
-    m_EffectSize *= 0.95;
 }
 
 void PanelDeleteEffect::Draw( int pixel_x, int pixel_y )
@@ -36,10 +33,13 @@ void PanelDeleteEffect::Draw( int pixel_x, int pixel_y )
         elapsed_ms = m_LifeTimeMs;
     }
 
-    double alpha = (1.0 - (static_cast<double>(elapsed_ms) / m_LifeTimeMs) / 2.0);
+    double t = (static_cast<double>(elapsed_ms) / m_LifeTimeMs);
     Vec2 drawpos(pixel_x + m_Move.x, pixel_y + m_Move.y);
+    double drawsize = (1.0 - t) * m_EffectSize;
 
-    s3d::Shape2D::Star(m_EffectSize, drawpos, 0.0).draw(s3d::ColorF(s3d::Palette::Yellow, alpha));
+    s3d::RenderStateBlock2D blend(BlendState::Additive);
+    s3d::Circle(drawpos, drawsize / 2.0).drawShadow(Vec2(0, 0), 3.0, 0, s3d::ColorF(s3d::Palette::Yellow));
+    s3d::Circle(drawpos, drawsize).drawShadow(Vec2(0, 0), 6.0, 0, s3d::ColorF(s3d::Palette::Yellow));
 }
 
 bool PanelDeleteEffect::IsFinished() const
